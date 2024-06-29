82690cf43052a62705a4fbcbd8e23a91b8b0f1ae
Demetrius <thedioxider@mail.ru>
2024-06-29 21:13:21 +0300

    feat(data): table classes are now singletons and have interface

--- lib/data/tables.dart
+++ lib/data/tables.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:hello_flutter/data/session.dart';
 import 'package:hive/hive.dart';
 
@@ -7,16 +9,38 @@ Future<void> initTables() async {
   ]);
 }
 
-class SleepSessionTable {
+abstract interface class Table<T> {
+  List<T> getValues();
+  putValue(T value);
+}
+
+class SleepSessionTable implements Table<SleepSession> {
   static const String name = 'sleep_session';
+
+  static SleepSessionTable? _instance;
+
+  static SleepSessionTable get instance {
+    if (_instance == null) {
+      init();
+      throw Exception('SleepSessionTable was not initialized');
+    }
+    return _instance!;
+  }
 
   SleepSessionTable._();
 
   static Future<void> init() async {
-    Hive.openBox(name);
+    await Hive.openBox(name);
+    _instance = SleepSessionTable._();
   }
 
-  static List<SleepSession> getValues() {
+  @override
+  List<SleepSession> getValues() {
     return Hive.box(name).values.map((e) => SleepSession.fromJson(e)).toList();
+  }
+
+  @override
+  putValue(SleepSession value) {
+    Hive.box(name).add(value.toJson());
   }
 }
